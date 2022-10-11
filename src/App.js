577cff308caba6f022917e54b38296cab9cd4a28
lilia <liliakai@gmail.com>
2022-10-11 12:47:38 +0200

    Built basic TicTacToe in 1 hour

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
+function Cell(props) {
+
+  function onClick() {
+    props.onClick(props.index);
+  }
+
+  return (
+    <div className="cell" onClick={onClick}>
+      { props.state }
+    </div>
+  );
+}
+
 function App() {
+  const [turn, setTurn] = useState("X");
+  const [board, setBoard] = useState(Array(9).fill(""));
+  const [winner, setWinner] = useState(null);
+
+  function didWin() {
+    const winStates = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (const i in winStates) {
+      const cells = winStates[i];
+      if (cells.every((i) => board[i] === turn)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  function onCellClick(i) {
+    if (board[i] !== "") {
+      return;
+    }
+    const b = board;
+    board[i] = turn;
+    setBoard(b);
+
+    if (didWin()) {
+      setWinner(turn);
+      return;
+    }
+
+    if (turn === 'X') {
+      setTurn("O");
+    } else if (turn === 'O') {
+      setTurn("X");
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          TicTacToe
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        { winner ? <p> Winner: {winner} </p> : null }
       </header>
+      <div className="content">
+        <div className="board">
+          {
+            board.map(function(cell, i) {
+              return (
+                <Cell key={i} index={i} state={cell} onClick={onCellClick} />
+              );
+            })
+          }
+        </div>
+      </div>
     </div>
   );
 }
